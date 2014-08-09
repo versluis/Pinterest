eb753ddfc5ad805e2e4451edd80e57d2ed0b37b8
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2014-08-09 16:52:44 -0400

    Initial Commit

---
+++ Pinterest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pinterest
+//
+//  Created by Jay Versluis on 09/08/2014.
+//  Copyright (c) 2014 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

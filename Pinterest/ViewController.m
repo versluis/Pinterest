e2ce88e13f1df3b2579804ed2438869d107ce659
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2014-08-09 17:48:04 -0400

    added Pinterest icon

--- Pinterest/ViewController.m
+++ Pinterest/ViewController.m
@@ -26,4 +26,47 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)openPinterest:(id)sender {
+    
+    // open the Pinterest App
+    if (![[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"pinterest://user/juliav1"]]) {
+        
+        // opening the app didn't work - let's open Safari
+        if (![[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://www.pinterest.com/versluis2000/"]]) {
+            
+            // nothing works - perhaps we're not online
+            NSLog(@"Dang!");
+        }
+    }
+}
+
+- (IBAction)openPinterestBoard:(id)sender {
+    
+    // open the Pinterest App
+    if (![[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"pinterest://board/versluis2000/ios-projects"]]) {
+        
+        // opening the app didn't work - let's open Safari
+        if (![[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://www.pinterest.com/versluis2000/ios-projects/"]]) {
+            
+            // nothing works - perhaps we're not online
+            NSLog(@"Dang!");
+        }
+    }
+}
+
+- (IBAction)openPinterestPin:(id)sender {
+    
+    // open the Pinterest App
+    if (![[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"pinterest://pin/76279787413881109"]]) {
+        
+        // opening the app didn't work - let's open Safari
+        if (![[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://www.pinterest.com/pin/76279787413881109/"]]) {
+            
+            // nothing works - perhaps we're not online
+            NSLog(@"Dang!");
+        }
+    }
+}
+
+
 @end
